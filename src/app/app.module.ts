d8e3a0d31b13f53b92ffe665f51932c5982cf8e1
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2021-04-06 09:10:23 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/shared/header/header.component';
+import { FooterComponent } from './componentes/shared/footer/footer.component';
+import { NavComponent } from './componentes/shared/nav/nav.component';
+import { ShoppingCartComponent } from './componentes/shopping-cart/shopping-cart.component';
+import { ProductListComponent } from './componentes/shopping-cart/product-list/product-list.component';
+import { CartComponent } from './componentes/shopping-cart/cart/cart.component';
+import { CartItemComponent } from './componentes/shopping-cart/cart/cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingCartComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
